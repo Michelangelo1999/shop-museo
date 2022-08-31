37cfc161af2147d9f165f86bd68c7bf91af1fbe2
MattiaVerardi <verardi.mattia97@gmail.com>
2022-08-31 15:28:36 +0200

    costruzione metodo crea

--- src/main/java/jana60/controller/ProdottoController.java
+++ src/main/java/jana60/controller/ProdottoController.java
@@ -1,11 +1,17 @@
 package jana60.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jana60.model.Prodotto;
 import jana60.repository.ProdottoRepo;
 
 @Controller
@@ -16,8 +22,24 @@ public class ProdottoController {
 	private ProdottoRepo repo;
 
 	@GetMapping
-	public String prodottoList(Model model) {
-		model.addAttribute("prodotto", repo.findAll());
-		return "/prodotto/list";
+	public String prodottoList(Model m) {
+		m.addAttribute("prodotto", repo.findAll());
+		return "/prodotto/lista";
+	}
+
+	@GetMapping("/crea")
+	public String prodottoCrea(Model m) {
+		m.addAttribute("prodotto", new Prodotto());
+		return "/prodotto/crea";
+	}
+
+	@PostMapping("/crea")
+	public String crea(@Valid @ModelAttribute("prodotto") Prodotto creaProdotto, BindingResult br) {
+		if (br.hasErrors()) {
+			return "/prodotto/crea";
+		} else {
+			repo.save(creaProdotto);
+			return "redirect:/";
+		}
 	}
 }

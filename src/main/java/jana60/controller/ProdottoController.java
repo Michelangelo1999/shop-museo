597b433e338d853b96f0c03b5d61e92a6bb391d9
MattiaVerardi <verardi.mattia97@gmail.com>
2022-08-31 15:57:34 +0200

    struttura base finita (da correggere modifica e elimina)

--- src/main/java/jana60/controller/ProdottoController.java
+++ src/main/java/jana60/controller/ProdottoController.java
@@ -1,15 +1,21 @@
 package jana60.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.model.Prodotto;
 import jana60.repository.ProdottoRepo;
@@ -21,14 +27,14 @@ public class ProdottoController {
 	@Autowired
 	private ProdottoRepo repo;
 
-	@GetMapping
+	@GetMapping("/")
 	public String prodottoList(Model m) {
 		m.addAttribute("prodotto", repo.findAll());
 		return "/prodotto/lista";
 	}
 
 	@GetMapping("/crea")
-	public String prodottoCrea(Model m) {
+	public String creaProdotto(Model m) {
 		m.addAttribute("prodotto", new Prodotto());
 		return "/prodotto/crea";
 	}
@@ -40,6 +46,29 @@ public class ProdottoController {
 		} else {
 			repo.save(creaProdotto);
 			return "redirect:/";
+		}
+	}
+
+	@GetMapping("/modifica/{id}")
+	public String modificaProdotto(@PathVariable("id") Integer prodottoId, Model model) {
+		Optional<Prodotto> result = repo.findById(prodottoId);
+		if (result.isPresent()) {
+			model.addAttribute("prodotto", result.get());
+			return "/prodotto/crea";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il prodotto " + prodottoId + " non è presente");
+		}
+	}
+
+	@GetMapping("/elimina/{id}")
+	public String eliminaProdotto(@PathVariable("id") Integer prodottoId, RedirectAttributes ra) {
+		Optional<Prodotto> result = repo.findById(prodottoId);
+		if (result.isPresent()) {
+			repo.delete(result.get());
+			ra.addFlashAttribute("successMessage", "Il prodotto " + result.get().getNome() + " è stato eliminato!");
+			return "redirect:/";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il prodotto " + prodottoId + " non è presente");
 		}
 	}
 }
